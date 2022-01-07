021c59b366307db2e36c465ca7304de346d34849
silasnazare <silasnazare@outlook.com.br>
2022-01-06 21:09:36 -0300

    testes de unidade

---
+++ DotNET-C#/Primeiro/Calculo/Calculo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primeiro
+{
+    public class Calculo
+    {
+        int Soma(int a, int b) { return a + b; }
+
+        int Subtracao(int a, int b) { return a - b; }
+
+        int Multiplicacao(int a, int b) { return a * b; }
+
+        int Divisao(int a, int b) { return a / b; }
+    }
+}
---
+++ DotNET-C#/Primeiro/Primeiro/Calculo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primeiro
+{
+    public class Calculo
+    {
+        public int Soma(int a, int b) { return a + b; }
+
+        public int Subtracao(int a, int b) { return a - b; }
+
+        public int Multiplicacao(int a, int b) { return a * b; }
+
+        public int Divisao(int a, int b) { return a / b; }
+    }
+}
